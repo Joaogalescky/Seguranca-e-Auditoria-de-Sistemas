67648323ecef1eeb494dd891aeb3dd6698a95cb3
Joaogalescky <joaocvgalescky@gmail.com>
2025-05-31 20:47:02 -0300

    feat(function): verificação de integridade

--- Gerador_Metadados_Verificacao/src/GeradorMetadados.py
+++ Gerador_Metadados_Verificacao/src/GeradorMetadados.py
@@ -2,6 +2,7 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.backends import default_backend
 import secrets
+import os
 
 def aes_cbc_cifrar(chave, iv, dados):
     # Criptografa os dados usando AES-CBC com padding PKCS7
@@ -37,3 +38,36 @@ def gerar_arquivos(caminho_arquivo, chave):
         f.write(header)
         
     return saida
+
+def verificar_integridade(caminho_arquivo, chave):
+    caminho_metadados = caminho_arquivo + ".meta"
+    
+    if not os.path.exists(caminho_metadados):
+        return "[ERRO] Arquivo .meta não encontrado!"
+    
+    with open(caminho_metadados, "rb") as f:
+        dados = f.read(48)
+        
+    if len(dados) != 48:
+        return "[ERRO] Arquivo .meta inválido!"
+    
+    ident = dados[0:2]
+    versao = dados[2]
+    algoritmo = dados[3]
+    modo = dados[4]
+    iv = dados[5:21]
+    fingerprint_armazenado = dados[21:37]
+    
+    if ident != b'CF' or versao != 1 or algoritmo != 1 or modo != 1:
+        return "[ERRO] Cabeçalho inválido ou incompátivel!"
+    
+    with open(caminho_arquivo, "rb") as f:
+        conteudo = f.read()
+        
+    novo_cifrar = aes_cbc_cifrar(chave, iv, conteudo)
+    novo_fingerprint = novo_cifrar[-16:]
+    
+    if novo_fingerprint == fingerprint_armazenado:
+        return "[SUCESSO] Integridade verificada: arquivo não foi modificado"
+    else:
+        return "[ATENÇÃO]! Arquivo foi modificado ou corrompido!"
