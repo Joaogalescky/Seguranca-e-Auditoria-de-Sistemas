b87d85de4d79bb86cf609fab0111e72a9b49c123
João Vitor Campõe Galescky <126702799+Joaogalescky@users.noreply.github.com>
2025-06-04 21:47:54 -0300

    style: GeradorMetadados

--- Gerador_Metadados_Verificacao/src/GeradorMetadados.py
+++ Gerador_Metadados_Verificacao/src/GeradorMetadados.py
@@ -11,12 +11,9 @@ def aes_cbc_cifrar(chave, iv, dados):
     # Criptografa os dados usando AES-CBC com padding PKCS7
     padder = padding.PKCS7(128).padder()
     padded = padder.update(dados) + padder.finalize()
-
-    cifrar = Cipher(algorithms.AES(chave), modes.CBC(iv),
-                    backend=default_backend())
+    cifrar = Cipher(algorithms.AES(chave), modes.CBC(iv), backend=default_backend())
     cifrador = cifrar.encryptor()
     texto_cifrado = cifrador.update(padded) + cifrador.finalize()
-
     return texto_cifrado
 
 
